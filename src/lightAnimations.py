55f970eef28cdf01894cda769c6876e8c3143a31
Zachary Rogers <zrogers>
2021-03-25 17:16:20 -0400

    Adding a rainbow feature

--- src/lightAnimations.py
+++ src/lightAnimations.py
@@ -25,3 +25,22 @@ def pingPong(pixels, num_pixels, timeInSeconds, color=(255,0,0)):
             pixels[i] = color
             pixels[i+1] = (0,0,0)
         time.sleep(stepInterval)
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+
+def unifiedRainbow(pixels, timeInSeconds):
+    stepInterval = 255 / timeInSeconds
+    for i in range(255):
+        pixels.fill(wheel(i))
+        time.sleep(stepInterval)
--- src/something.py
+++ src/something.py
@@ -11,36 +11,6 @@ ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=True, pixel_order=ORDER)
 #  Green->Red->Blue
  
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        return (0, 0, 0)
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    if pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    pos -= 170
-    return (pos * 3, 0, 255 - pos * 3)
- 
- 
-def color_chase(color, wait):
-    for i in range(num_pixels):
-        pixels[i] = color
-        time.sleep(wait)
-        pixels.show()
-    time.sleep(0.5)
- 
- 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(rc_index & 255)
-        pixels.show()
-        time.sleep(wait)
- 
  
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
@@ -52,3 +22,4 @@ PURPLE = (180, 0, 255)
 while True:
     countdown(pixels, 3)
     pingPong(pixels, num_pixels, 3, YELLOW)
+    unifiedRainbow(pixels, 3)
