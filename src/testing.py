ea8e4b960322e0032934afb9c914de7cd30a8fbf
ZacharyRogers <ztrogers1@gmail.com>
2021-04-04 17:27:07 -0400

    Trying to use RxPy as a way of stopping animations midway

--- src/testing.py
+++ src/testing.py
@@ -1,12 +1,32 @@
-import board
-import neopixel
-from animations import twoDAnimations
+# import board
+# import neopixel
+# from animations import twoDAnimations
 
-pixels = neopixel.NeoPixel(board.D18, 50, auto_write=False)
+# pixels = neopixel.NeoPixel(board.D18, 50, auto_write=False)
+
+# # while True:
+# #     translate2DPointTo1DPosition(pixels, 4, 3, (30,0,0))
+# #     translate2DPointTo1DPosition(pixels, 9, 4, (0,30,0))
 
 # while True:
-#     translate2DPointTo1DPosition(pixels, 4, 3, (30,0,0))
-#     translate2DPointTo1DPosition(pixels, 9, 4, (0,30,0))
+#     twoDAnimations.scanningStripe(pixels, 10, 5, 0, (155,255,89))
 
-while True:
-    twoDAnimations.scanningStripe(pixels, 10, 5, 0, (155,255,89))
+
+from rx import of, subject
+
+hello = of(1,2,3,'hello')
+doot = subject.Subject()
+
+
+listener1 = doot.subscribe( lambda value : print("listening to subject: ", value))
+
+doot.on_next('yo')
+terrible = 'terrible'
+
+hello.subscribe( lambda value : print(value, terrible))
+
+# Make all of state a behavior subject
+
+# Create a new subject every time a new animation starts so that way you could publish a stop command to the function
+
+# Create a universal way to stop animations
