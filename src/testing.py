a757783e4d32f46007fde90a31bbfd0181a8339a
Zachary Rogers <zrogers>
2021-04-11 18:35:39 -0400

    WIP testing rxpy with multithreading

--- src/testing.py
+++ src/testing.py
@@ -2,33 +2,65 @@
 # import neopixel
 from animations import twoDAnimations, lightAnimations
 from rx import of, subject, operators
+from rx.scheduler import ThreadPoolScheduler
+from threading import current_thread
 import time
-
-
-# pixels = neopixel.NeoPixel(board.D18, 50, auto_write=False)
+import multiprocessing
+import random
 
 shouldIContinueSubject = subject.BehaviorSubject(True)
 
-shouldIContinue = True
+def intense_calculation(value):
+    # sleep for a random short duration between 0.5 to 2.0 seconds to simulate a long-running calculation
+    time.sleep(random.randint(5, 20) * 0.1)
+    return value
 
-def change(shouldIContinue2):
-    print("Subscriber #1: ", shouldIContinue2)
-    time.sleep(5)
-    print("Subscriber 1 finished waiting")
-    # global shouldIContinue
-    shouldIContinue = shouldIContinue2
 
-def change2(idk):
-    print("subscriber #2: ", idk)
+# calculate number of CPUs, then create a ThreadPoolScheduler with that number of threads
+optimal_thread_count = multiprocessing.cpu_count()
+pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
+print("OPTIMAL THREADS: ", optimal_thread_count)
 
-shouldIContinueSubject.pipe(operators.delay(0.001)).subscribe(change)
-shouldIContinueSubject.subscribe(change2)
+# Create Process 1
+shouldIContinueSubject.pipe(
+    operators.map(lambda s: intense_calculation(s))
+).subscribe(
+    on_next=lambda s: print("PROCESS 1: {0} {1}".format(current_thread().name, s)),
+    on_error=lambda e: print(e),
+    on_completed=lambda: print("PROCESS 1 done!"),
+    scheduler=operators.subscribe_on(pool_scheduler)
+)
+
+# Create Process 2
+shouldIContinueSubject.pipe(
+    operators.map(lambda s: intense_calculation(s))
+).subscribe(
+    on_next=lambda i: print("PROCESS 2: {0} {1}".format(current_thread().name, i)),
+    on_error=lambda e: print(e),
+    on_completed=lambda: print("PROCESS 2 done!"),
+    scheduler=operators.subscribe_on(pool_scheduler)
+)
+
+
+
+# def change(shouldIContinue2):
+#     print("Subscriber #1: ", shouldIContinue2)
+#     time.sleep(5)
+#     print("Subscriber 1 finished waiting")
+#     # global shouldIContinue
+#     shouldIContinue = shouldIContinue2
+
+# def change2(idk):
+#     print("subscriber #2: ", idk)
+
+# shouldIContinueSubject.subscribe(change)
+# shouldIContinueSubject.subscribe(change2)
 for i in range(10000000):
-    if(shouldIContinue == False):
-        print('Exiting Early: ', i)
-        break
     if(i == 8000):
         shouldIContinueSubject.on_next(False)
+    if(i == 8001):
+        shouldIContinueSubject.on_next(True)
+
 print("finished for loop")
 
 while True:
