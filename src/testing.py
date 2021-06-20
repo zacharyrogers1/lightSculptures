5cd3bf1778d6cd308390080dbc20e788590bf7e6
Zachary Rogers <zrogers>
2021-06-20 19:14:59 -0400

    t

--- src/testing.py
+++ src/testing.py
@@ -5,7 +5,7 @@ pixel_pin = board.D18
 num_pixels = 50
 ORDER = neopixel.RGB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER)
-pixels.brightness = 0.5
+pixels.brightness = 0.1
 while(True):
     pixels.fill((255,0,0))
     pixels.show()
