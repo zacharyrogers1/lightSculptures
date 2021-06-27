0c9506d5205408531109ced70f992ec327b99ee4
Zachary Rogers <zrogers>
2021-06-27 17:56:18 -0400

    testing using a lookup table

--- src/testing.py
+++ src/testing.py
@@ -8,10 +8,20 @@ ORDER = neopixel.RGB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER)
 pixels.brightness = 0.5
 while(True):
-    lightAnimations.twinkle(pixels, num_pixels, 1.0, (255,0,0))
+    lightAnimations.twinklePreCalc(pixels, num_pixels, 1.0, (255,0,0))
     # pixels[0:3] = [(255,0,0),(255,0,0),(255,0,0)]
     # pixels.show()
 
-# someList = [9,22,32]
-# someList[:3] = [5,5,5]
-# print(someList)
+
+# import random
+
+# numberOfSteps = 20
+# num_pixels = 50
+# brightnessLookup = [float(x)/numberOfSteps for x in range(numberOfSteps)]
+# startingPoints = [random.randint(0, numberOfSteps-1) for x in range(num_pixels)]
+# for i in range(numberOfSteps):
+#     newSpot = startingPoints[0]+i
+#     if(newSpot >= numberOfSteps):
+#         newSpot = newSpot - numberOfSteps
+#     brightness = brightnessLookup[newSpot]
+#     print(brightness)
