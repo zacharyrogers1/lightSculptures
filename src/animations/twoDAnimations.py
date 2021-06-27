b519b926f7a219262335065dd7fa43417a4edc56
Zachary Rogers <zrogers>
2021-06-27 12:24:43 -0400

    removed yAxisLength from scanning stripe

--- src/animations/twoDAnimations.py
+++ src/animations/twoDAnimations.py
@@ -3,14 +3,15 @@ from animations import animationHelpers
 # from animations.animationHelpers import *
 
 
-def scanningStripe(pixels, xAxisLength, yAxisLength, speed, color):
+def scanningStripe(pixels, xAxisLength, speed, color):
     maxSleepInterval = 1
     normalizedSpeed = animationHelpers.getNormalizedSpeed(speed, maxSleepInterval)
 
+    yAxisLength = int(pixels.n / xAxisLength)
     screen = animationHelpers.createBlankScreen(xAxisLength, yAxisLength)
     for x in range (xAxisLength):
         for y in range(yAxisLength):
-            screen[x][y] = (0,0,255)
+            screen[x][y] = color
         animationHelpers.show2DimensionalDisplay(pixels, screen)
         screen = animationHelpers.createBlankScreen(xAxisLength, yAxisLength)
         time.sleep(normalizedSpeed)
