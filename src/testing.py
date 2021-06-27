47c591d31b14ee876874d8f30fc1fb2a0d10d533
Zachary Rogers <zrogers>
2021-06-27 15:32:47 -0400

    t

--- src/testing.py
+++ src/testing.py
@@ -9,25 +9,9 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=
 pixels.brightness = 0.5
 while(True):
     # lightAnimations.twinkle(pixels, num_pixels, 0.0, (255,0,0))
-    # pixels = [(255,0,0), (255,0,0), (255,0,0)]
-    pixels[0] = (255,0,0)
+    pixels[0:2] = [(255,0,0),(255,0,0),(255,0,0)]
     pixels.show()
 
-
-# import math
-# import random
-# pixelCount = 20
-# startingBrightnessValues = [
-#     random.random()*2*math.pi for i in range(pixelCount)]
-# numberOfSteps = 10
-# stepSize = 2.0*math.pi/numberOfSteps
-
-# # print(startingBrightnessValues)
-
-# def brightnessEquation(brightness, stepSize, iterator):
-#     return (math.cos(brightness + stepSize*iterator) + 1)*0.5
-
-# for i in range(numberOfSteps+1):
-#     actualBrightness = [brightnessEquation(brightness, stepSize, i) for brightness in startingBrightnessValues]
-#     # startingBrightnessValues = [x + stepSize for x in startingBrightnessValues]
-#     print(actualBrightness)
+# someList = [9,22,32]
+# someList[:3] = [5,5,5]
+# print(someList)
