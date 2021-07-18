903d29eeedefd100d99a6319090f8b31dee1bc45
Zachary Rogers <zrogers>
2021-07-18 18:20:34 -0400

    t - full brightness 400

--- src/testing2.py
+++ src/testing2.py
@@ -7,8 +7,8 @@ pixel_pin = board.D18
 num_pixels = 400
 ORDER = neopixel.RGB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER)
-pixels.brightness = 0.3
-pixels.fill((0,255,0))
+pixels.brightness = 1.0
+pixels.fill((255,255,255))
 pixels.show()
 while(True):
     time.sleep(1)
