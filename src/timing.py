f5ca19f376a734d92880d3b625c88796a4a94efc
ZacharyRogers <ztrogers1@gmail.com>
2021-08-01 17:08:03 -0400

    Adding timing python test

---
+++ src/timing.py
@@ -0,0 +1,21 @@
+import board
+import neopixel
+import time
+from animations import lightAnimations, twoDAnimations
+
+pixel_pin = board.D18
+num_pixels = 400
+ORDER = neopixel.RGB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER)
+pixels.brightness = 0.3
+average = 0
+try:
+    while(True):
+        start = time.time()
+        pixels.fill((255,0,0))
+        pixels.show()
+        final = time.time()
+        timeElapsed = final-start
+        average = (average + timeElapsed)/2.0
+except:
+    print("Average Time: ", average)
