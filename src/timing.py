fb8e21f6b38e4d686e4fc0202cec7e6f9dd86ca1
ZacharyRogers <ztrogers1@gmail.com>
2021-08-01 17:15:05 -0400

    t

--- src/timing.py
+++ src/timing.py
@@ -9,14 +9,20 @@ ORDER = neopixel.RGB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER)
 pixels.brightness = 0.3
 average = 0
+flip = True
 try:
     while(True):
         start = time.time()
-        pixels.fill((255,0,0))
-        pixels.show()
+        if(flip):
+            pixels.fill((255,0,0))
+            pixels.show()
+        else:
+            pixels.fill((0,255,0))
+            pixels.show()
         final = time.time()
         timeElapsed = final-start
         average = (average + timeElapsed)/2.0
+        flip = not flip
 except:
     print("Average Time: ", average)
     print("Number Of Pixels: ", num_pixels)
