4478f1f76646cc7e7d33500cf934575e34bd8316
Zachary Rogers <zrogers>
2021-10-24 14:15:29 -0400

    WIP - changing how animations are done

--- src/testing2.py
+++ src/testing2.py
@@ -8,12 +8,6 @@ num_pixels = 400
 ORDER = neopixel.RGB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER)
 pixels.brightness = 1.0
+redColor = [255,0,0]
 while(True):
-    pixels.fill((255,0,0))
-    pixels.show()
-    pixels.fill((0,255,0))
-    pixels.show()
-    pixels.fill((0,0,255))
-    pixels.show()
-    pixels.fill((255,255,255))
-    pixels.show()
+    lightAnimations.chasingLights(pixels, 50, redColor, 0.1)
