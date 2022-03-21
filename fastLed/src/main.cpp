b7cd3b4af791ed9c40998d2f368122dbf4d4b0fe
zacharyrogers1 <zacharyrogers1@hotmail.com>
2022-03-20 18:55:42 -0700

    Added fastLED to project file

--- fastLed/src/main.cpp
+++ fastLed/src/main.cpp
@@ -1,19 +1,23 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 60
+#define DATA_PIN 6
+CRGB leds[NUM_LEDS];
 
 void setup()
 {
-  // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
-// the loop function runs over and over again forever
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   Serial.println(millis());
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                    // wait for a second
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
   Serial.println("LOW");
-  delay(1000);                     // wait for a second
+  delay(1000); // wait for a second
 }
