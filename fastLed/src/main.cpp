e21cf1711510b22f51c467ec7f87847d10c2419e
zacharyrogers1 <zacharyrogers1@hotmail.com>
2022-12-04 14:14:06 -0800

    Basic setup of FastLED with light net

--- fastLed/src/main.cpp
+++ fastLed/src/main.cpp
@@ -1,23 +1,21 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS 60
-#define DATA_PIN 6
+#define NUM_LEDS 400
+#define DATA_PIN 10
 CRGB leds[NUM_LEDS];
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  Serial.println(millis());
-  delay(1000);                    // wait for a second
-  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-  Serial.println("LOW");
-  delay(1000); // wait for a second
+  // leds[0] = CRGB::Red;
+  leds[1] = CRGB::Green;
+  // leds[2] = CRGB::Blue;
+  FastLED.show();
 }
