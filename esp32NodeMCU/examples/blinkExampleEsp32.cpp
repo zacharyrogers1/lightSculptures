ee028cdeb6d93e6c81615a04cc56f91de3c6a292
zacharyrogers1 <zacharyrogers1@hotmail.com>
2022-12-11 17:52:58 -0800

    Created example of working ESP32 which blinks the entire light string

---
+++ esp32NodeMCU/examples/blinkExampleEsp32.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 400
+#define DATA_PIN 27
+#define CHIPSET WS2811Controller800Khz
+#define BRIGHTNESS 128
+#define COLOR_CORRECTION TypicalPixelString
+#define ONBOARD_LED 2
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  pinMode(ONBOARD_LED, OUTPUT);
+  pinMode(DATA_PIN, OUTPUT);
+  Serial.begin(9600);
+  FastLED.addLeds<CHIPSET, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.setCorrection(COLOR_CORRECTION); // After some experimenting TypicalPixelString matches the white color correction best
+}
+
+void loop()
+{
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  digitalWrite(ONBOARD_LED, HIGH);
+  FastLED.show();
+  delay(1000);
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  digitalWrite(ONBOARD_LED, LOW);
+  FastLED.show();
+  delay(1000);
+}
+
